cdd0e84e3d0c48ca38aadfe1976567e924aa94a6
emirhalici <emir.halici1@gmail.com>
2022-04-01 14:05:50 +0300

    add basic settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iftarnezaman/models/city_model.dart';
+import 'package:iftarnezaman/providers/main_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -16,8 +21,29 @@ class _SettingsScreenState extends State<SettingsScreen> {
         title: const Text("Ayarlar"),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Column(
-        children: [],
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.save),
+          onPressed: () {
+            // save the values
+            Navigator.pop(context);
+          }),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Şehir",
+              style: TextStyle(fontSize: 32.sp),
+            ),
+            SizedBox(height: 16.h),
+            Text(
+              "İlçe",
+              style: TextStyle(fontSize: 32.sp),
+            ),
+          ],
+        ),
       ),
     );
   }
